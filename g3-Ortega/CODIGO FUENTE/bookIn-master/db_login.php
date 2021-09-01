48da1be2137c3315581b989b0d0507a62d8e099d
ITSGGYE <41635533+ITSGGYE@users.noreply.github.com>
2021-09-01 15:08:20 -0500

    Delete g3-Ortega directory

--- g3-Ortega/CODIGO FUENTE/bookIn-master/db_login.php
+++
@@ -1,7 +0,0 @@
-<?php
-	//variables de conexion
-	$db_host='200.105.245.250';
-	// $db_host= 'localhost';
-	$db_username='bvera';
-	$db_password='bvera';
-?>
--- g3-Ortega/CODIGO FUENTE/bookIn-master/qrgenerator.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // generador de qr
-    include('phpqrcode/qrlib.php');
-        
-    $param = $_GET['id']; // remember to sanitize that - it is user input!
-    
-    QRcode::png($param);
-?>
